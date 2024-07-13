80b7958bc4d603b6ae9be622ba7705fde680b0ae
Ibrahimjay <jallohim130@gmail.com>
2024-07-13 23:03:22 +0000

    solve secrete key error on deployment

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import home_view
 
 urlpatterns = [
-    path('', home_page ),
+    path('', home_view ),
     path('admin/', admin.site.urls),
 ]
